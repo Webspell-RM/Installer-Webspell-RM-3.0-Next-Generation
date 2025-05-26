2a1506a2695e7336b2b4a1f8a65a23d52eb22fdc
T-Seven <info@designperformance.de>
2025-05-26 22:40:45 +0200

    settings update

--- install/data/admin_user.sql
+++ install/data/admin_user.sql
@@ -7,8 +7,15 @@ VALUES ('', 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO `contact` (`name`, `email`, `sort`) VALUES
 ('Administrator', '{{adminmail}}', 1);
 
-INSERT IGNORE INTO `settings` (`hptitle`, `hpurl`, `clanname`, `clantag`, `adminname`, `adminemail`, `sball`, `topics`, `posts`, `latesttopics`, `latesttopicchars`, `messages`, `register_per_ip`, `sessionduration`, `closed`, `imprint`, `default_language`, `insertlinks`, `search_min_len`, `max_wrong_pw`, `captcha_math`, `captcha_bgcol`, `captcha_fontcol`, `captcha_type`, `captcha_noise`, `captcha_linenoise`, `bancheck`, `spam_check`, `detect_language`, `spammaxposts`, `spamapiblockerror`, `date_format`, `time_format`, `modRewrite`, `startpage`, `forum_double`, `profilelast`, `de_lang`, `en_lang`, `it_lang`, `birthday`, `keywords`, `description`) VALUES
-('Webspell-RM', '{{adminweburl}}', 'Clan Name', 'MyClan', '{{adminuser}}', '{{adminmail}}', 30, 20, 10, 10, 18, 20, 1, 0, 0, 1, 'de', 1, 3, 10, 2, '#FFFFFF', '#000000', 2, 100, 10, 1564938159, 0, 0, 0, 0, 'd.m.Y', 'H:i', 0, 'startpage', 1, 10, 1, 1, 1, 0, 'Clandesign, Webspell, Webspell-RM, Wespellanpassungen, Webdesign, Tutorials, Downloads, Webspell-rm, rm, addon, plugin, Templates Webspell Addons, plungin, mods, Webspellanpassungen, Modifikationen und Anpassungen und mehr!', 'Kostenlose Homepage erstellen mit Webspell-RM CMS: Einfach, schnell & kostenlos! In wenigen Minuten mit der eigenen Website online gehen.');
+INSERT INTO `settings` (
+  `hptitle`, `hpurl`, `clanname`, `clantag`, `adminname`, `adminemail`, `since`,
+  `webkey`, `seckey`, `closed`, `default_language`, `de_lang`, `en_lang`, `it_lang`,
+  `keywords`, `description`, `modRewrite`, `startpage`
+) VALUES (
+  'Webspell-RM', '{{adminweburl}}', 'Mein Clan / Verein', '[RM]', '{{adminuser}}', '{{adminmail}}', 2025,
+  'PLACEHOLDER_WEBKEY', 'PLACEHOLDER_SECKEY', 0, 'de', 1, 1, 1,
+  'Clandesign, Webspell, Webspell-RM, Wespellanpassungen, Webdesign, Tutorials, Downloads, Webspell-rm, rm, addon, plugin, Templates Webspell Addons, plungin, mods, Webspellanpassungen, Modifikationen und Anpassungen und mehr!', 'Kostenlose Homepage erstellen mit Webspell-RM CMS: Einfach, schnell & kostenlos! In wenigen Minuten mit der eigenen Website online gehen.', 0, 'startpage'
+);
 
 INSERT IGNORE INTO `user_username` (`userID`, `username`) VALUES (1, '{{adminuser}}');
 
